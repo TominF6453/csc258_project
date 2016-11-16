7f1765034e17c50047ab84986f82188f4c17bc30
br3nd4nn34l <br3nd4nn34l@gmail.com>
2016-11-16 17:57:49 -0500

    Made module for the actual implementation of the timer on the board.

---
+++ ActualTimer.v
@@ -0,0 +1,19 @@
+`include "AbstractConventionalTimer.v"
+
+module ActualTimer(
+    input CLOCK_50,
+    output [6:0] HEX4,
+    output [6:0] HEX3,
+    output [6:0] HEX2,
+    output [6:0] HEX1,
+    output [6:0] HEX0);
+
+	AbstractConventionalTimer ActTimer(
+		.clock(CLOCK_50),
+    	.HEXfour(HEX4),
+    	.HEXthree(HEX3),
+    	.HEXtwo(HEX2),
+    	.HEXone(HEX1),
+    	.HEXzero(HEX0);
+    
+endmodule
