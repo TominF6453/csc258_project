4d817380715c9f1a157bd40130d0d45212bb8428
br3nd4nn34l <br3nd4nn34l@gmail.com>
2016-11-16 17:22:07 -0500

    Added a counter that counts 5,4,..0,5,..

---
+++ six_counter.v
@@ -0,0 +1,17 @@
+`include "bit4_counter.v"
+
+/*Counter that counts in this order:
+5,4,3,2,1,0,5,4..., on every posedge of in_pulse. 
+Outputs a pulse on out_pulse whenever it resets to 0*/
+module six_counter(
+    input in_pulse,
+    output out_pulse,
+    output [3:0] cur_value);
+
+	bit4_counter ctr (
+		.in_pulse(in_pulse),
+		.start(4'd6),
+		.out_pulse(out_pulse),
+		.pos_value(cur_value));
+    
+endmodule
